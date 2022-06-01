f6d39587d020b5568f9a0a50de55819239c1894f
David Pascall <davidp@laptop.mrc-bsu.cam.ac.uk>
2022-06-01 11:53:49 +0100

    Fixed double stochasticity bug

--- Rcode/bitstringgeneration.R
+++ Rcode/bitstringgeneration.R
@@ -1,6 +1,7 @@
 ##sim code
 library(MASS)
-library(utils)
+library(R.utils)
+library(gtools)
 
 ##function for generating bitstrings
 generatebitstring <- function(mu, omega, pos, n) {
@@ -9,22 +10,24 @@ generatebitstring <- function(mu, omega, pos, n) {
   
   ##set up object to hold bitstrings 
   strings <- rep(NA, length = n)
+  positions <- rep(NA, length = n)
   
-  for (i in 1:n) {
-    ##generate MVN variates on logit scale
-    logittarget <- mvrnorm(n = 1, logitmu, omega, tol = 1e-06, empirical = FALSE)
-    
+  ##generate MVN variates on logit scale
+  logittarget <- mvrnorm(n = n, logitmu, omega, tol = 1e-06, empirical = FALSE)
+  logittarget <- ifelse(logittarget > 0, 1, 0)
+  
+  for (i in 1:n) { 
     ##apply inverse logistic function
-    target <- plogis(logittarget)
     targetextend <- rep(0, 29900)
-    targetextend[pos[,1]] <- rbinom(rep(1, length(target)), rep(1, length(target)), target)
+    targetextend[pos[,1]] <- logittarget[i,]
     
     ##save bitstring
     strings[i] <- paste(targetextend, collapse = "")
+    positions[i] <- paste(pos[logittarget[i,], 1], collapse = ",")
   }
   
   ##output bitstrings
-  return(strings)
+  return(list(strings,positions))
 }
 
 ##function for handling uncompression and file I/O
@@ -38,10 +41,10 @@ run <- function(n) {
   #for each location
   for (j in 1:length(locations)) {
     ##read in probabily vector
-    mu <- as.vector(read.table(paste0(getwd(), "/", locations[j], "/", "Variant_frequencies", filename[j], ".out"), header = F, sep = " "))
+    mu <- read.table(paste0(getwd(), "/", locations[j], "/", "Variant_frequencies", filename[j], ".out"), header = F, sep = " ")
     
     ##unzip correlation matrix
-    untar(paste0(getwd(), "/", locations[j], "/", "Variant_correlations", filename[j], ".out.gz"), list = FALSE, exdir = paste0(getwd(), "/", locations[j]), compressed = T)
+    gunzip(filename = paste0(getwd(), "/", locations[j], "/", "Variant_correlations", filename[j], ".out.gz"), destname = paste0(getwd(), "/", locations[j], "/", "Variant_correlations", filename[j], ".out"), remove = FALSE)
     
     ##read in correlation matrix
     omega <- as.matrix(read.table(paste0(getwd(), "/", locations[j], "/", "Variant_correlations", filename[j], ".out"), header = F, sep = " "))
@@ -56,7 +59,8 @@ run <- function(n) {
     bitstrings <- generatebitstring(mu, omega, pos, n)
     
     ##write out
-    write.table(bitstrings, file = paste0(getwd(), "/", "bitstrings", filename[j], ".txt"), row.names = F, col.names = F, sep = "\t", eol = "\n")
+    write.table(bitstrings[[1]], file = paste0(getwd(), "/", "bitstrings", filename[j], ".txt"), row.names = F, col.names = F, sep = "\t", eol = "\n")
+    write.table(bitstrings[[1]], file = paste0(getwd(), "/", "positions", filename[j], ".txt"), row.names = F, col.names = F, sep = "\t", eol = "\n")
   }
 }
 
